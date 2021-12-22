f0e8af88b0125c6d75c2e520938a5c91e6105047
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-12-22 13:07:34 +0700

    Banking apps error fixed!
    No URIDecode error comes up with other plugins.

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,13 @@ function deleteCookie(cname) {
     document.cookie = cname + "=;" + expires + ";path=/";
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split('; ');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
     return "";
 }
